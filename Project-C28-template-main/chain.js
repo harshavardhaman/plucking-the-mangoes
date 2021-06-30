5abf4058f5fe7b1dc442a362dc4074f1bfc8c701
harshavardhaman <73707112+harshavardhaman@users.noreply.github.com>
2021-06-30 03:44:42 -1200

    Add files via upload

---
+++ Project-C28-template-main/chain.js
@@ -0,0 +1,28 @@
+class Slingshot{
+    constructor(bodyA,pointB){
+        var options={
+            bodyA:bodyA,
+            pointB:pointB,
+            stiffness:0.2,
+            length:10
+        }
+        this.pointB=pointB
+        this.sling=Constraint.create(options);
+        World.add(world,this.sling);
+    }fly(){
+        this.sling.bodyA=null;
+    }
+    attach(body){ 
+        this.sling.bodyA=body
+    }
+    display(){
+        if(this.sling.bodyA){
+
+        
+        var pointA=this.sling.bodyA.position;
+        var pointB=this.pointB;
+        strokeWeight(5);
+        line(pointA.x,pointA.y-50,pointB.x,pointB.y);
+        }
+    }
+}
